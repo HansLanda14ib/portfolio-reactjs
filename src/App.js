e8c4f24a92c966daff66cd03f9248a773bcc0f1e
Hansa Landa <bibzazne@gmail.com>
2023-10-14 13:48:43 +0100

    add notify for submit ContactMe.jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
 import Home from "./Pages/Home/Homescreen";
-
+import Notiflix from "notiflix";
+Notiflix.Notify.init({
+  position: 'left-top', // Notification position
+  distance: '10px', // Distance between notifications
+  opacity: 1, // Notification opacity
+  borderRadius: '12px', // Border radius
+  fontFamily: 'inherit', // Font family
+  fontSize: '18px', // Font size
+  timeout: 1000,
+});
 function App() {
   return (
     <div className="App">
--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -1,87 +1,91 @@
 import emailjs from '@emailjs/browser';
 import {useRef} from "react";
+import {Notify} from "notiflix";
+
 
 export default function ContactMe() {
-  const form = useRef();
-  const sendEmail = (e) => {
-    e.preventDefault();
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_jumcdjm', 'template_0yydyw3', form.current, 'AuSkL9D26DsmEvD3X')
+            .then((result) => {
+                console.log(result.text);
+                Notify.success("Email has been sent successfully");
+            }, (error) => {
+                console.log(error.text);
+                Notify.failure("Error sending email");
+            });
 
-    emailjs.sendForm('service_jumcdjm', 'template_0yydyw3', form.current, 'AuSkL9D26DsmEvD3X')
-        .then((result) => {
-          console.log(result.text);
-        }, (error) => {
-          console.log(error.text);
-        });
+        e.target.reset()
+    };
+    return (
+        <section id="Contact" className="contact--section">
+            <div>
+                <p className="sub--title">Get In Touch</p>
+                <h2>Contact Me</h2>
+                <p className="text-lg">
 
-  };
-  return (
-    <section id="Contact" className="contact--section">
-      <div>
-        <p className="sub--title">Get In Touch</p>
-        <h2>Contact Me</h2>
-        <p className="text-lg">
+                    Send me a message here, or at <strong>badreddineibzazne@gmail.com</strong>.
+                </p>
+            </div>
+            <form ref={form} className="contact--form--container" onSubmit={sendEmail}>
+                <div className="container">
+                    <label htmlFor="first-name" className="contact--label">
+                        <span className="text-md">First Name</span>
+                        <input
+                            type="text"
+                            className="contact--input text-md"
+                            name="first-name"
+                            id="first-name"
+                            required
+                        />
+                    </label>
+                    <label htmlFor="last-name" className="contact--label">
+                        <span className="text-md">Last Name</span>
+                        <input
+                            type="text"
+                            className="contact--input text-md"
+                            name="last-name"
+                            id="last-name"
+                            required
+                        />
+                    </label>
+                    <label htmlFor="email" className="contact--label">
+                        <span className="text-md">Email</span>
+                        <input
+                            type="email"
+                            className="contact--input text-md"
+                            name="email"
+                            id="email"
+                            required
+                        />
+                    </label>
+                    <label htmlFor="phone-number" className="contact--label">
+                        <span className="text-md">phone number</span>
+                        <input
+                            type="number"
+                            className="contact--input text-md"
+                            name="phone-number"
+                            id="phone-number"
+                            required
+                        />
+                    </label>
+                </div>
 
-          Send me a message here, or at <strong>badreddineibzazne@gmail.com</strong>.
-        </p>
-      </div>
-      <form ref={form} className="contact--form--container" onSubmit={sendEmail}>
-        <div className="container">
-          <label htmlFor="first-name" className="contact--label">
-            <span className="text-md">First Name</span>
-            <input
-              type="text"
-              className="contact--input text-md"
-              name="first-name"
-              id="first-name"
-              required
-            />
-          </label>
-          <label htmlFor="last-name" className="contact--label">
-            <span className="text-md">Last Name</span>
-            <input
-              type="text"
-              className="contact--input text-md"
-              name="last-name"
-              id="last-name"
-              required
-            />
-          </label>
-          <label htmlFor="email" className="contact--label">
-            <span className="text-md">Email</span>
-            <input
-              type="email"
-              className="contact--input text-md"
-              name="email"
-              id="email"
-              required
-            />
-          </label>
-          <label htmlFor="phone-number" className="contact--label">
-            <span className="text-md">phone-number</span>
-            <input
-              type="number"
-              className="contact--input text-md"
-              name="phone-number"
-              id="phone-number"
-              required
-            />
-          </label>
-        </div>
+                <label htmlFor="message" className="contact--label">
+                    <span className="text-md">Message</span>
+                    <textarea
+                        className="contact--input text-md"
+                        name="message"
+                        rows="8"
+                        placeholder="Type your message..."
+                    />
+                </label>
 
-        <label htmlFor="message" className="contact--label">
-          <span className="text-md">Message</span>
-          <textarea
-            className="contact--input text-md"
-            name="message"
-            rows="8"
-            placeholder="Type your message..."
-          />
-        </label>
-
-        <div>
-          <button className="btn btn-primary contact--form--btn">Submit</button>
-        </div>
-      </form>
-    </section>
-  );
+                <div>
+                    <button className="btn btn-primary contact--form--btn">Submit</button>
+                </div>
+            </form>
+        </section>
+    );
 }
