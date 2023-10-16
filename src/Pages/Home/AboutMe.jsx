ca01df99e7a4ad4dc287d087cb98e6c749eae64d
Hansa Landa <bibzazne@gmail.com>
2023-10-16 12:33:28 +0100

    correction tranduction partie a propos de moi

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -8,36 +8,48 @@ export default function AboutMe() {
         english: {
             title: 'About',
             heading: 'About Me',
-            description: `Currently in my final year at EMSI Marrakesh, where I have been diligently expanding my expertise in Computer Engineering. I hold a Master's degree in Materials Science and have successfully transitioned into the field of Software Development, with valuable experience and newfound skills.
-
-In addition to my studies, I am also working as the Education Manager at the CPGE IbnGhazi center, where I oversee various educational programs and initiatives. Additionally, I serve as a Python tutor at the same center, where I contribute to fostering a strong understanding of programming concepts among the students. In these roles, I have the opportunity to apply my leadership skills and contribute to the growth and success of the center, nurturing an environment that promotes learning and development.`,
-        },
-        french: {
-            title: 'À propos',
-            heading: 'À propos de moi',
-            description: `Actuellement en dernière année à l'EMSI Marrakech, où j'élargis diligemment mon expertise en l'Ingénierie Informatique. Je suis titulaire d'un master en science des matériaux et j'ai réussi à me reconvertir dans le domaine du développement de logiciels, avec une expérience précieuse et de nouvelles compétences.
-
-En plus de mes études, je travaille également en tant que responsable de l'éducation au centre CPGE IbnGhazi, où je supervise différents programmes éducatifs et initiatives. De plus, je suis tuteur en Python au même centre, où je contribue à favoriser une bonne compréhension des concepts de programmation chez les étudiants. Dans ces rôles, j'ai l'opportunité d'appliquer mes compétences en leadership et de contribuer à la croissance et au succès du centre, en favorisant un environnement qui encourage l'apprentissage et le développement.`,
+            description: `I am currently in my final year at EMSI Marrakech, focusing on Computer Engineering and Networks. 
+            With a Master's degree in Materials Science, I successfully transitioned into Software Development.`,
+            description2: `Alongside my studies, I worked as an IT manager at CPGE Ibn Ghazi center, overseeing all technology-related functions. Previously, 
+            I was the education manager at the same center, supporting educational programs and ensuring effective learning for students and adequate resources for teachers. 
+           `,
+            description3: ` In these roles, I apply leadership skills, contributing to the center's growth and fostering a supportive learning environment.`
+        }, french: {
+            title: 'À propos', heading: 'À propos de moi',
+            description: `
+            Je suis titulaire d'un master en sciences des matériaux et je suis actuellement en 5ème année - ingénierie informatique et réseaux à l'EMSI Marrakech.`,
+            description2: `
+            En parallèle de mes études, j'occupe le poste de Responsable Informatique au centre CPGE Ibn Ghazi, supervisant toutes les fonctions liées à la technologie. 
+            Auparavant, j'ai été Rresponsable Scolarité dans le même centre, soutenant les programmes éducatifs et veillant à un apprentissage efficace pour les étudiants ainsi qu'à des ressources adéquates pour les enseignants. 
+            
+            `,
+            description3: `Dans ces rôles, j'applique des compétences en leadership, contribuant à la croissance du centre et favorisant un environnement d'apprentissage favorable.`
         },
     };
 
-    return (
-        <section id="AboutMe" className="about--section">
-            <div className="about--section--box">
-                <div className="hero--section--content">
-                    <p className="section--title">{content[language].title}</p>
-                    <h1 className="skills-section--heading">{content[language].heading}</h1>
-                    <p className="hero--section-description">
-                        {content[language].description}
+    return (<section id="AboutMe" className="about--section">
+        <div className="about--section--box">
+            <div className="hero--section--content">
+                <p className="section--title">{content[language].title}</p>
+                <h1 className="skills-section--heading">{content[language].heading}</h1>
+                <p className="hero--section-description">
+                    {content[language].description}
 
-                    </p>
+                </p>
+                <p className="hero--section-description">
+                    {content[language].description2}
 
-                </div>
+                </p>
+                <p className="hero--section-description">
+                    {content[language].description3}
+
+                </p>
+
             </div>
-            <div className="about--section--img">
-                <img src="./aboutme.png" alt="About Me"/>
-            </div>
+        </div>
+        <div className="about--section--img">
+            <img src="./aboutme.png" alt="About Me"/>
+        </div>
 
-        </section>
-    );
+    </section>);
 }
